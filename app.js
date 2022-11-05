11c12c6edc67afd113498bc1f0464cdc70ae78df
Mahdi <mahdivafaii1999@gmail.com>
2022-11-05 19:03:07 +0330

    replace correct files

--- app.js
+++ app.js
@@ -1,109 +1,75 @@
-const app = document.querySelector(".weather-app");
-const temp = document.querySelector(".temp");
-const dataOutput = document.querySelector(".data");
-const timeOutput = document.querySelector(".time");
-const conditionOutput = document.querySelector(".condition");
-const nameOutput = document.querySelector(".name");
-const iconOutput = document.querySelector(".icon");
-const cloudOutput = document.querySelector(".cloud");
-const humidityOutput = document.querySelector(".humidity");
-const windOutput = document.querySelector(".wind");
-const form = document.getElementById('locationInput');
-const search = document.querySelector('.search');
-const btn = document.querySelector('.submit');
-const cities = document.querySelectorAll('.city');
+//api key: fc38d11bdd7a5956e133fb2adb0360d1;
+//select elements
 
-let cityInput = "London";
+const iconElement = document.querySelector('.weather.icon')
+const tempElement = document.querySelector('.temperature-value p')
+const descElement = document.querySelector('.temperature-description p')
+const locationElement = document.querySelector('.location p')
+const notificationElement = document.querySelector('.notification')
 
-console.log("hi");
+//app data
 
-cities.forEach((city) => {
-    city.addEventListener('click', (e) => {
-        cityInput = e.target.innerHTML;
-        fetchWeatherData();
-        app.style.opacity = "0";
-    });
-})
-
-form.addEventListener('submit', (e) => {
-    if(search.value.length == 0) {
-        alert("Please type a city name");
-    }else {
-        cityInput = search.value;
-        fetchWeatherData();
-        search.value = "";
-        app.style.opacity = "0";
-    }
-
-    e.preventDefault();
-});
-
-function dayOfTheWeek(day, month, year) {
-    const weekday = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-    ];
-    return weekday[new Date(`$(day)/$(month)/$(year)`).getDay()];
+const weather = {};
+weather.temperature = {
+    unit: 'Celsius'
 };
 
-function fetchWeatherData(){
-    const KEY = b40341c664da4a52bda195646220411
-    fetch(`http://api.weatherapi.com/v1/current.json?key=b40341c664da4a52bda195646220411&q=${cityInput}&aqi=no`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            temp.innerHTML = data.current.temp_c + "&#176";
-            conditionOutput.innerHTML = data.current.condition.text;
-            const date = data.location.localtime;
-            const y = parseInt(date.substr(0,4));
-            const m = parseInt(date.substr(5,2));
-            const d = parseInt(date.substr(8,2));
-            const time = date.substr(11);
+//const and variables
+const KELVIN = 273;
 
-            dateOutput.innerHTML = `${dayOfTheWeek(d, m, y)} ${d} ${m} ${y}`;
-            timeOutput.innerHTML = time;
-            nameOutput.innerHTML = data.location.name;
-            cloudOutput.innerHTML = data.current.cloud + "%";
-            humidityOutput.innerHTML = data.current.humidity + "%";
-            windOutput.innerHTML = data.current.wind_kph + "km/h";
+//api key
+const key = "fc38d11bdd7a5956e133fb2adb0360d1";
 
-            let timeOfDay = "day";
-            const code = data.current.condition.code;
+//check browser geolocation
+if ('geolocation' in navigator){
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}else{
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML = "<p> Browser Doesn't support Geolocation </p>";
+}
 
-            if(!data.current.is_day) {
-                timeOfDay = "night";
-            }
+//set position
+function setPosition(position){
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
 
-            if(code == 1000) {
-                app.style.backgroundImage = `url(./images/${timeOfDay}/clear.jpg`;
-                btn.stylebackground = "#e5ba92";
-                if(timeOfDay == "night") {
-                    btn.style.background = "#181e27";
-                }
-            }
-            else if (
-                code == 1003 ||
-                code == 1006 ||
-                code == 1009
-            ){
-                app.style.backgroundImage = `url(./images/${timeOfDay}/cloudy)`;
-                btn.style.background = "#fa6d1b"
-                if(timeOfDay == "night") {
-                    btn.style.background = "#181e27";
-                }
-            }
-            app.style.opacity = "1";
+    getWeather(latitude, longitude);
+}
+
+//Show Error when there is a problem with geolocation
+
+function showError(error){
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
+}
+
+//get weather from API
+
+function getWeather(latitude,longitude){
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
+    fetch(api)
+        .then(function (response){
+           let data = response.json();
+           return data;
         })
-        .catch(() => {
-            alert("Try Again");
-            app.style.opacity = "1";
+        .then(function (data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+            console.log(data);
+        })
+        .then(function(){
+          displayWeather();
         });
 }
 
-fetchWeatherData();
-app.style.opacity = "1";
+//display weather to UI
+
+function displayWeather(){
+    //iconElement.innerHTML = `<img src='icons/${weather.iconId}.png/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
